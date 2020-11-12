59ea51a0ef8d35334f1036a244fd39cf6a7fd730
Nick Kosik <Nicholas.kosik@gmail.com>
2020-11-11 19:00:15 -0600

    added serialize

--- services/passport.js
+++ services/passport.js
@@ -5,12 +5,29 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id); // done is callback when we've finished work
+    // (error obj or if we ran into errors, identify user for follow-up requests)
+});
+
 passport.use(new GoogleStrategy({ 
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save();
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id })
+            .then(existingUser => {
+                if(existingUser) {
+                    // already have record with given profile ID
+                    done(null, existingUser); // provide 2 args (if error, user record)
+                } else {
+                    // dont have user record with this ID, make new record
+                    new User({ googleId: profile.id }) // creates mongoose model instance
+                        .save() // save instance
+                        .then(user => done(null, user)); // in callback get another model instance
+                }
+            });
         }
     )
 ); 
